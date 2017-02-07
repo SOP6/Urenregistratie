3a8c7a49b348f3c85ec3164d5f79736a30e190ab
Loek Delahaye <loek.delahaye@coosto.com>
2017-02-07 22:00:40 +0100

    fixed a glyphicon error running npm run watch.

--- public/js/app.js
+++ public/js/app.js
@@ -31621,7 +31621,7 @@ var Component = __webpack_require__(35)(
   /* cssModules */
   null
 )
-Component.options.__file = "/home/skywalker/Documents/Projects/Urenregistratie/urenregistratie_new/resources/assets/js/components/Example.vue"
+Component.options.__file = "/home/loek/Documents/Projects/Urenregistratie/resources/assets/js/components/Example.vue"
 if (Component.esModule && Object.keys(Component.esModule).some(function (key) {return key !== "default" && key !== "__esModule"})) {console.error("named exports are not supported in *.vue files.")}
 if (Component.options.functional) {console.error("[vue-loader] Example.vue: functional components are not supported with templates, they should use render functions.")}
 
@@ -31632,9 +31632,9 @@ if (false) {(function () {
   if (!hotAPI.compatible) return
   module.hot.accept()
   if (!module.hot.data) {
-    hotAPI.createRecord("data-v-371e35af", Component.options)
+    hotAPI.createRecord("data-v-2ff24a86", Component.options)
   } else {
-    hotAPI.reload("data-v-371e35af", Component.options)
+    hotAPI.reload("data-v-2ff24a86", Component.options)
   }
 })()}
 
@@ -31719,7 +31719,7 @@ module.exports.render._withStripped = true
 if (false) {
   module.hot.accept()
   if (module.hot.data) {
-     require("vue-hot-reload-api").rerender("data-v-371e35af", module.exports)
+     require("vue-hot-reload-api").rerender("data-v-2ff24a86", module.exports)
   }
 }
 
